4cc863bfff3e159be27fd19433c496b34c36b86a
root <136849653@qq.com>
2019-05-10 18:05:21 +0800

    用于测试是否能上传

---
+++ src/test/java/com/yidong/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Test
+ * Author:   18811
+ * Date:     2019/5/10 18:04
+ * Description: 用于测试是否能上传
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.yidong;
+
+public class Test {
+}
